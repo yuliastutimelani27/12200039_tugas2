ca0aa17297675e9f90e5b216b1836121e7949d31
yuliastutimelani27 <yuliastutimelani1@gmail.com>
2021-09-25 13:48:38 +0700

    menambahkan biodata_tugas2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama Saya Yuli Astuti Melani
+			Saya tinggal di alamat Dsn Cihideung 1 Desa Budiasih RT 02 RW 01 Sindangkasih
+			No HP 085211720420
+			olah raga yang saya sukai adalah
+			Bulutangkis";
+	}
+}
